e43dc66837875353fefefcc1e5acd7c6ac05f53d
Gourishetty Pranay <pranaygourishetti@gmail.com>
2024-03-11 18:52:29 +0530

    Add files via upload

---
+++ Dicee Challenge - Starting Files/di.js
@@ -0,0 +1,4 @@
+var image = ['dice1.png', 'dice2.png', 'dice3.png', 'dice4.png', 'dice5.png', 'dice6.png']
+var randomNumber1 = Math.floor(Math.random()*image.lenght)
+document.getElementById("dice1") = randomNumber1;
+alert("what is your name");
---
+++ Java script/first.js
@@ -0,0 +1,7 @@
+var a = prompt("Enter your name");
+var b = confirm("U still want to continue");
+if (b == true) {
+    document.write("HI " +a )
+} else {
+    document.write("Error" + a)
+}
